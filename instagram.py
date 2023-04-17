ef7991966a76f558d22a7b9605c1172c60b6b951
Daniel Suárez <dwsuarez98@gmail.com>
2023-04-17 19:08:39 -0300

    Inicio

---
+++ instagram.py
@@ -0,0 +1,157 @@
+# Import statements
+import selenium
+from selenium import webdriver 
+from selenium.webdriver.common.by import By 
+from selenium.webdriver.common.keys import Keys 
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from time import sleep 
+import wget
+import getpass
+
+#  Instagram User data
+my_user = "sofia.punk_ph"
+target = "winna_z"
+#my_pwd =  getpass.getpass()
+my_psw = "9silverstone8"
+
+
+#  Opciones
+options = webdriver.ChromeOptions()
+options.add_argument('--start-maximized')
+options.add_argument('--disable-extensions')
+options.add_experimental_option('detach', True)
+
+# Output dir 
+dest_loc = '/home/daniel/Escritorio/temp/sell/sell3/images/'
+
+# Driver init
+# driver = webdriver.Chrome('/var/task/scraper/chromedriver/chromedriver', options=chrome_options)
+driver = webdriver.Chrome(options=options)
+driver.get("https://instagram.com/")
+
+#################
+# User & Password
+#################
+WebDriverWait(driver, 12).until(
+        EC.element_to_be_clickable((
+            By.XPATH, '//*[@id="loginForm"]/div/div[1]/div/label/input'
+            ))
+        ).click()
+
+WebDriverWait(driver, 12).until(
+        EC.element_to_be_clickable((
+            By.XPATH, '//*[@id="loginForm"]/div/div[1]/div/label/input'
+            ))
+        ).send_keys(my_user)
+
+
+WebDriverWait(driver, 12).until(
+        EC.element_to_be_clickable((
+            By.XPATH, '//*[@id="loginForm"]/div/div[2]/div/label/input'
+            ))
+        ).click()
+
+
+WebDriverWait(driver, 12).until(
+        EC.element_to_be_clickable((
+            By.XPATH, '//*[@id="loginForm"]/div/div[2]/div/label/input'
+            ))
+        ).send_keys(my_psw)
+
+
+#################
+# Iniciar session
+#################
+WebDriverWait(driver, 12).until(
+        EC.element_to_be_clickable((
+            By.XPATH, '//*[@id="loginForm"]/div/div[3]/button/div'
+            ))
+        ).click()
+
+
+
+WebDriverWait(driver, 12).until(
+        EC.element_to_be_clickable((
+            By.CLASS_NAME, 'xa49m3k'
+            ))
+        ).click()
+
+WebDriverWait(driver, 12).until(
+        EC.element_to_be_clickable((
+            By.XPATH, '/html/body/div[2]/div/div/div[3]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[2]'
+            ))
+        ).click()
+#####################
+# Search 
+#####################
+WebDriverWait(driver, 12).until(
+        EC.element_to_be_clickable((
+            By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[1]/div/div/div/div/div[2]/div[2]/div/a/div/div[1]/div/div'
+            ))
+        ).click()
+
+WebDriverWait(driver, 20).until(
+        EC.element_to_be_clickable((
+            By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[1]/div/div/div[2]/div/div/div[2]/div[1]/div/input'
+            ))
+        ).click()
+
+
+busqueda = WebDriverWait(driver, 20).until(
+        EC.element_to_be_clickable((
+            By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[1]/div/div/div[2]/div/div/div[2]/div[1]/div/input'
+            ))
+        ).send_keys(target)
+
+# Seleccionar la búsqueda
+WebDriverWait(driver, 20).until(
+        EC.element_to_be_clickable((
+            By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[1]/div/div/div[2]/div/div/div[2]/div[2]/div/div[1]/a/div/div/div/div[2]/div/div'
+            ))
+        ).click()
+
+#############################
+# Encontrar y recopilar links
+#############################
+my_images = set()
+last_height = 0
+
+while True:
+    # Obtener la altura actual de la página
+    new_height = driver.execute_script("return document.body.scrollHeight")
+    
+    # Scroll hacia abajo
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    
+    # Encontrar y guardar los links
+    images = driver.find_elements(By.CSS_SELECTOR,"._aagv img")
+    for image in images:
+        try:
+            source = image.get_attribute("src")
+        except:
+            continue
+        my_images.add(source)
+    
+    sleep(3)
+    
+    # Verificar si se han cargado todas las imágenes
+    if new_height == last_height:
+        print("Se han cargado todas las imágenes")
+        break
+    
+    # Actualizar la altura anterior
+    last_height = new_height
+
+print(len(my_images))
+
+########################
+# Descargar las imágenes
+########################
+for image in my_images:
+    try:
+        wget.download(image,dest_loc)
+    except:
+        continue
+
+driver.quit()
