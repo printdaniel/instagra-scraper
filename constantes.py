52b6c3536a2d4c47ce856f37fe65e68d475374b9
Daniel <dwsuarez98@gmail.com>
2023-08-22 14:52:25 -0300

    logging agreado

--- constantes.py
+++ constantes.py
@@ -1,7 +1,6 @@
 # Usuario
-USER = 'xxxx'
+USER = 'your_user'
 TARGET = 'target'
-
 
 #################
 # User & Password
@@ -15,15 +14,13 @@ LOGIN_2 = "//*[@id='loginForm']/div/div[2]/div/label/input"
 INIT_SESSION_1 = "/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[3]/button/div"
 INIT_SESSION = "/html/body/div[2]/div/div/div[3]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[2]"
 
-
 #####################
 # Search 
 #####################
-SEARCH_1 = "/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[1]/div/div/div/div/div[2]/div[2]/span/div/a/div/div[2]/div/div/span/span"
+SEARCH_1 = "/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[1]/div/div/div/div/div[2]/div[2]/span/div/a/div/div[1]/div/div"
 SEARCH_2 = "/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[1]/div/div/div[2]/div/div/div[2]/div/div/div[1]/div/div/input"
 
 #################
 # Select
 #################
 SELECT = "/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[1]/div/div/div[2]/div/div/div[2]/div/div/div[2]/div/a[1]/div[1]/div/div/div[2]/div/div/span/span"
-
--- instagram.py
+++ instagram.py
@@ -10,20 +10,27 @@ from time import sleep
 import wget
 import getpass
 import os
+import logging
+
+# Configuración básica del logger
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# Crear un objeto logger
+logger = logging.getLogger(__name__)
 
 #  Instagram User data
 my_user = USER
 target = TARGET
 #my_pwd =  getpass.getpass()
 my_psw = getpass.getpass()
+logger.info("Credenaciales cargadas")
 
-
-print(USER)
 #  Opciones
 options = webdriver.ChromeOptions()
 options.add_argument('--start-maximized')
 options.add_argument('--disable-extensions')
 options.add_experimental_option('detach', True)
+logger.info("Opciones [webdriver, maximized, detach]")
 
 # Output dir 
 current_dir = os.getcwd()
@@ -31,54 +38,63 @@ dest_loc = current_dir + '/images/'
 
 if not os.path.exists(dest_loc):
     os.makedirs(dest_loc)
+logger.info("Directorio de imágenes creado")
 
 # Driver init
 # driver = webdriver.Chrome('/var/task/scraper/chromedriver/chromedriver', options=chrome_options)
 driver = webdriver.Chrome(options=options)
 driver.get("https://instagram.com/")
+logger.info("Chromium cargado")
 
 #################
 # User & Password
 #################
 WebDriverWait(driver, 12).until(EC.element_to_be_clickable((
             By.XPATH, LOGIN_1 ))).click()
+logger.info("User paso 1")
 
 WebDriverWait(driver, 12).until(EC.element_to_be_clickable((
             By.XPATH, LOGIN_1 ))).send_keys(my_user)
-
+logger.info("User paso 2")
 
 WebDriverWait(driver, 12).until(
         EC.element_to_be_clickable((
             By.XPATH, LOGIN_2 ))).click()
-
+logger.info("User paso 3")
 
 WebDriverWait(driver, 12).until(EC.element_to_be_clickable((
         By.XPATH, LOGIN_2))).send_keys(my_psw)
-
+logger.info("User paso 4")
 
 #################
 # Iniciar session
 #################
 WebDriverWait(driver, 12).until(EC.element_to_be_clickable((
     By.XPATH, INIT_SESSION_1 ))).click()
+logger.info("Inicio de sesión paso 1")
 
 WebDriverWait(driver, 12).until(EC.element_to_be_clickable((
     By.CLASS_NAME, 'xa49m3k'))).click()
+logger.info("Inicio de sesión paso 2")
 
 WebDriverWait(driver, 12).until(EC.element_to_be_clickable((
     By.XPATH, INIT_SESSION ))).click()
+logger.info("Inicio de sesión paso 3")
 
 #####################
 # Search 
 #####################
 WebDriverWait(driver, 12).until(EC.element_to_be_clickable((
         By.XPATH, SEARCH_1 ))).click()
+logger.info("Inicio de search paso 1")
 
 WebDriverWait(driver, 20).until(EC.element_to_be_clickable((
         By.XPATH, SEARCH_2 ))).click()
+logger.info("Inicio de search paso 2")
 
 busqueda = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((
         By.XPATH, SEARCH_2 ))).send_keys(target)
+logger.info("Inicio de search paso 3")
 
 #########################
 # Seleccionar la búsqueda
